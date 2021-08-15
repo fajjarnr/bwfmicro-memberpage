08fedef918a96814d226ae0ffd686a82c8edba31
fajjarnr <zfajart@gmail.com>
2021-08-15 12:15:53 +0700

    error page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { createBrowserHistory } from "history";
 import { Route, Router, Switch } from "react-router-dom";
-import GuestRoutes from "./components/Routes/GuestRoutes";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
 
 import "assets/css/style.css";
+
+import GuestRoutes from "./components/Routes/GuestRoutes";
 import MemberRoute from "components/Routes/MemberRoute";
-import MyClass from "pages/MyClass";
+
+import NotFound from "pages/404";
 import Unauthenticated from "pages/401";
+
+import Login from "./pages/Login";
 import Register from "pages/Register";
+import MyClass from "pages/MyClass";
 
 function App() {
   const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
